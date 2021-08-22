f1e48f10099c1a2a623d120769bf2030fa4c13a9
Konrad Czerw <redmastersuper@gmail.com>
2021-08-22 15:37:27 +0200

    i think i'm done???

---
+++ client.js
@@ -0,0 +1,14 @@
+const { WebSocket } = require('ws');
+const { Server } = require('net');
+
+const client = new WebSocket('ws://127.0.0.1:8080');
+
+client.on('open', () => {
+	let server = new Server();
+	server.on('connection', s => {
+		s.on('close', () => { client.close(); server.close(); })
+		s.on('data', d => client.send(d));
+		client.on('message', m => s.write(m));
+	})
+	server.listen(process.argv[2] || 25565, '127.0.0.1');
+});
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const { Server } = require('ws');
-const spawn = require('child_process').spawn;
-const path = require('path');
+const { WebSocketServer } = require('ws');
+const { Socket } = require('net');
+const { createServer } = require('http');
+const { spawn } = require('child_process');
 
-const port = process.env.PORT || 3000;
-const s = express().use((req, res) => res.sendFile(req.path || 'index.html', { root: path.join(__dirname, 'public') })).listen(port, () => console.log('listening on ' + port));
-
-const wss = new Server({ server: s });
-wss.on('connection', (ws) => {
-	ws.on('message', m => {
-		const a = m.toString().replace(/\n+$/, '').split(' ');
-		const p = spawn(a.shift() || '', a, { stdio: ['pipe', 'pipe', 'pipe'] });
-		p.on('error', console.error);
-		p.stdout.setEncoding('utf-8').on('data', s => ws.send(s.replace(/\n+$/, '')));
-		p.stderr.setEncoding('utf-8').on('data', s => ws.send(s.replace(/\n+$/, '')));
-	});
+const httpServer = createServer((req, res) => {
+	const body = 'dummy server.';
+	res.writeHead(404, { 'Content-Length': Buffer.byteLength(body), 'Content-Type': 'text/plain' }).end(body);
 });
 
-process.stdin.on('data', b => {
-	
+const server = new WebSocketServer({ server: httpServer });
+
+server.on('connection', s => {
+	let client = new Socket();
+	client.connect(25565, '127.0.0.1');
+	s.on('close', () => client.destroy());
+	s.on('message', m => client.write(m));
+	client.on('data', d => s.send(d));
 });
+
+httpServer.listen(8080 || process.env.PORT);
+
+spawn('./jdk-16.0.2/bin/java', '-jar server.jar -Xmx2G --nogui'.split(' '));
